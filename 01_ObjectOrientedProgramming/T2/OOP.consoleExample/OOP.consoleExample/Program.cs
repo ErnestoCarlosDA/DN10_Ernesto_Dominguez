98942e063cd05cca29e5ff595a2e8ad3c4fa0b5b
ERNESTO CARLOS DOMINGUEZ ALONSO <ernesto28agosto2000@hotmail.com>
2023-06-28 10:18:31 -0600

    Tutorial2
    Codigo_del_tutorial2

---
+++ 01_ObjectOrientedProgramming/T2/OOP.consoleExample/OOP.consoleExample/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AudioManager;
+namespace OOP.consoleExample
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("type the animal");
+
+
+                String animaType = Console.ReadLine().ToLower();
+                Animal animal = null;
+                switch (animaType)
+                {
+                    case "cat":
+                        animal = new Cat();
+                        break;
+                    case "cow":
+                        animal = new Cow();
+                        break;
+                    case "dog":
+                        animal = new Dog();
+                        break;
+                    case "elephant":
+                        animal = new Elephant();
+                        break;
+                    case "lion":
+                        animal = new Lion();
+                        break;
+                    case "pig":
+                        animal = new Pig();
+                        break;
+
+                    default:
+                        Console.WriteLine("Animal no faund");
+                        break;
+                }
+
+                if(animal != null)
+                {
+                    animal.AnimalSoud();
+                }
+
+            }
+            
+    }
+}
+}
