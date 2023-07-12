ae6027b792a3888a7a80ae80f97e5b2d48090db3
ERNESTO CARLOS DOMINGUEZ ALONSO <ernesto28agosto2000@hotmail.com>
2023-07-12 12:57:20 -0600

    Tutorial1
    tutorial1 video

---
+++ 01_ObjectOrientedProgramming/T1/OOP.consoleExample1/AudioManager/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioManager
+{
+    public class Animal
+    {
+        public void Animalsound()
+        {
+            Console.WriteLine("i am animal sound");
+        }
+    }
+}
---
+++ 01_ObjectOrientedProgramming/T1/OOP.consoleExample1/OOP.consoleExample1/Program.cs
@@ -0,0 +1,22 @@
+﻿using AudioManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.consoleExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Animal animal = new Animal();
+            animal.Animalsound();
+
+
+            Console.Read();
+        }
+    }
+}
